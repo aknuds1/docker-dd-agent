57c35578a20595b9dea8bc88d4fa8ffed1436783
Arve Knudsen <arve.knudsen@gmail.com>
2015-12-27 14:35:56 +0100

    Handle RethinkDB connectivity issues

--- checks.d/rethinkdb.py
+++ checks.d/rethinkdb.py
@@ -9,18 +9,25 @@ class RethinkDBCheck(AgentCheck):
     def check(self, instance):
         self.log.info('Checking RethinkDB connectivity...')
         rethinkdb_host = os.environ.get('RETHINKDB_HOST', 'localhost')
-        with r.connect(host=rethinkdb_host, db='muzhack') as conn:
-            projects = r.table('projects').run(conn)
-        if projects:
-            self.log.info('RethinkDB works, successfully queried projects')
-            msg_title = 'RethinkDB is functioning'
-            msg_text = 'RethinkDB was successfully queried for data'
-            alert_type = 'info'
-        else:
-            self.log.info('RethinkDB doesn\'t work, couldn\t query projects')
-            msg_title = 'RethinkDB returns no data'
-            msg_text = 'RethinkDB did not return any projects'
+        try:
+            with r.connect(host=rethinkdb_host, db='muzhack') as conn:
+                projects = r.table('projects').run(conn)
+        except r.ReqlError as err:
+            self.log.warn('Connecting to RethinkDB @ {} failed: {}'.format(rethinkdb_host, err))
+            msg_title = 'RethinkDB is not connectable'
+            msg_text = 'RethinkDB did not respond on adress {}'.format(rethinkdb_host)
             alert_type = 'error'
+        else:
+            if projects:
+                self.log.info('RethinkDB works, successfully queried projects')
+                msg_title = 'RethinkDB is functioning'
+                msg_text = 'RethinkDB was successfully queried for data'
+                alert_type = 'info'
+            else:
+                self.log.info('RethinkDB doesn\'t work, couldn\t query projects')
+                msg_title = 'RethinkDB returns no data'
+                msg_text = 'RethinkDB did not return any projects'
+                alert_type = 'error'
         self.event({
             'timestamp': int(time.time()),
             'event_type': 'rethinkdb_check',
