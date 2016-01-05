3d46fe01572cbb9f1fff5e3840bef9f43f6bd13f
Arve Knudsen <arve.knudsen@gmail.com>
2016-01-05 13:58:26 +0100

    Style fixes

--- checks.d/rethinkdb.py
+++ checks.d/rethinkdb.py
@@ -1,4 +1,4 @@
-    #!/usr/bin/env python
+#!/usr/bin/env python
 from checks import AgentCheck
 import time
 import rethinkdb as r
@@ -11,12 +11,17 @@ class RethinkDBCheck(AgentCheck):
         rethinkdb_host = os.environ.get('RETHINKDB_HOST', 'localhost')
         rethinkdb_auth_key = os.environ['RETHINKDB_AUTH_KEY']
         try:
-            with r.connect(host=rethinkdb_host, db='muzhack', auth_key=rethinkdb_auth_key) as conn:
+            with r.connect(
+                    host=rethinkdb_host, db='muzhack',
+                    auth_key=rethinkdb_auth_key) as conn:
                 projects = r.table('projects').run(conn)
         except r.ReqlError as err:
-            self.log.warn('Connecting to RethinkDB @ {} failed: {}'.format(rethinkdb_host, err))
+            self.log.warn(
+                'Connecting to RethinkDB @ {} failed: {}'
+                    .format(rethinkdb_host, err))
             msg_title = 'RethinkDB is not connectable'
-            msg_text = 'RethinkDB did not respond on adress {}'.format(rethinkdb_host)
+            msg_text = 'RethinkDB did not respond on adress {}'.format(
+                rethinkdb_host)
             alert_type = 'error'
         else:
             if projects:
@@ -25,7 +30,8 @@ class RethinkDBCheck(AgentCheck):
                 msg_text = 'RethinkDB was successfully queried for data'
                 alert_type = 'info'
             else:
-                self.log.info('RethinkDB doesn\'t work, couldn\t query projects')
+                self.log.info(
+                    'RethinkDB doesn\'t work, couldn\t query projects')
                 msg_title = 'RethinkDB returns no data'
                 msg_text = 'RethinkDB did not return any projects'
                 alert_type = 'error'
